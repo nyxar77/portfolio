4af218e556b529760c34b62f703395cb318a4ce9
fe2-Nyxar <nyxar49@gmail.com>
2026-03-21 03:07:14 +0000

    new build file

---
+++ build.ts
@@ -0,0 +1,13 @@
+import tailwind from "bun-plugin-tailwind";
+
+await Bun.build({
+  entrypoints: ["./src/index.html"],
+  outdir: "./dist",
+  sourcemap: "external",
+  target: "browser",
+  minify: true,
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+  plugins: [tailwind],
+});
