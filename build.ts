73f5f2edb2095cec1b27288e16c40497b47b3842
fe2-Nyxar <nyxar49@gmail.com>
2026-06-02 12:52:20 +0100

    changed the resumes

--- build.ts
+++ build.ts
@@ -14,9 +14,9 @@ await Bun.build({
 
 await Bun.write(
   "./dist/resume/resume-en.pdf",
-  Bun.file("./resume/resume-en.pdf"),
+  Bun.file("./resume/Abderrahmane_Drissi_Resume_EN.pdf"),
 );
 await Bun.write(
   "./dist/resume/resume-fr.pdf",
-  Bun.file("./resume/resume-fr.pdf"),
+  Bun.file("./resume/Abderrahmane_Drissi_CV_FR.pdf"),
 );
